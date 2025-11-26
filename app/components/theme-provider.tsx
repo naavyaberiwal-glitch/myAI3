3f58a75e891ad919a4ab719613902bc0714a90c5
naavyaberiwal-glitch <naavyaberiwal@gmail.com>
2025-11-26 21:09:09 +0530

    Update theme-provider.tsx

--- app/components/theme-provider.tsx
+++ app/components/theme-provider.tsx
@@ -3,14 +3,14 @@
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
-type Props = {
-  children: React.ReactNode;
-};
-
-export function ThemeProvider({ children, ...props }: Props & React.ComponentProps<typeof NextThemesProvider>) {
-  // defaultTheme "system" lets users switch between light/dark
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
   return (
-    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem {...(props as any)}>
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem={true}
+      disableTransitionOnChange
+    >
       {children}
     </NextThemesProvider>
   );
